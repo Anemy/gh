063731d97401cd908fad8872062eb1998b7772d2
Rhys Howell <rhysh@live.com>
2017-12-20 06:42:16 -0500

    added cname

--- src/components/TopBar/TopBar.js
+++ src/components/TopBar/TopBar.js
@@ -9,37 +9,42 @@ import welshFlagSVG from './Flag_of_Wales.svg';
 const TopBar = () => (
   <div className="top-bar">
     <div className="top-bar-items">
-      <img
-        alt=""
-        className="top-bar-profile-pic"
-        src="/pic-1.jpg"
-      />
-      <div className="top-bar-title-name">
-        Gareth Howell
-      </div>
-      <div className="top-bar-nav-area">
-        <NavLink
-          activeClassName="top-bar-nav-link-active"
-          className="top-bar-nav-link"
-          exact
-          to="/"
-        >
-          Wall
-        </NavLink>
-        <NavLink
-          activeClassName="top-bar-nav-link-active"
-          className="top-bar-nav-link"
-          to="/links"
-        >
-          Links
-        </NavLink>
-        <NavLink
-          activeClassName="top-bar-nav-link-active"
-          className="top-bar-nav-link"
-          to="/info"
-        >
-          Info
-        </NavLink>
+      <div className="g">
+        <div className="u-3-24" />
+        <div className="u-6-24">
+          <img
+            alt=""
+            className="top-bar-profile-pic"
+            src="/pic-1.jpg"
+          />
+          <div className="top-bar-title-name">
+            Gareth Howell
+          </div>
+        </div>
+        <div className="u-8-24 top-bar-nav-area">
+          <NavLink
+            activeClassName="top-bar-nav-link-active"
+            className="top-bar-nav-link"
+            exact
+            to="/"
+          >
+            Wall
+          </NavLink>
+          <NavLink
+            activeClassName="top-bar-nav-link-active"
+            className="top-bar-nav-link"
+            to="/links"
+          >
+            Links
+          </NavLink>
+          <NavLink
+            activeClassName="top-bar-nav-link-active"
+            className="top-bar-nav-link"
+            to="/info"
+          >
+            Info
+          </NavLink>
+        </div>
       </div>
       <img src={welshFlagSVG} className="top-bar-welsh-flag" alt="" />
       <img src={usaFlagSVG} className="top-bar-usa-flag" alt="" />
