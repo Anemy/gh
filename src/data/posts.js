a4bdd6e06249493327b75712813cc3c7c1a73350
Rhys Howell <rhysh@live.com>
2017-12-27 22:37:00 -0500

    New posts

--- src/data/posts.js
+++ src/data/posts.js
@@ -11,6 +11,60 @@ export default [{
   familyPost: true,
   text: 'Hello all, Gareth is now starting hospice care at home. During this season of family time we invite you to share your best wishes, memories, and photos. <br>Love from all of us.'
 }, {
+  author: 'Rick and Elisabeth', // Waugaman
+  date: 'December 27th, 2017',
+  text: 'It has been such a privilege for Elisabeth and I to get to know Gareth and his family. The way we first met will remain lovingly engraved in our memory. At a time when I was feeling deeply discouraged about a certain anti-intellectual element in a private club I\'d belonged to for many years, this cheerful Welshman showed up, challenging the unspoken taboo against questioning who wrote the works of a certain famous author. He then proceeded to work through the channels to get the Board\'s approval for a newly monthly luncheon group, to discuss the forbidden topic. Soon, his group was drawing as many attendees as the pre-existing orthodox group. <br>Gareth succeeds where others fail because of his unique combination of intelligence, determination, wide knowledge, and over-the-top people skills. Everyone loves him (except those intent on shutting down open discussion of a controversial topic).<br>We were always delighted the many times Gareth came to our home for our informal "movie nights," with lively discussions afterwards. He always contributed in his deeply knowledgeable and good-humored way.<br>Gareth, you will be remembered fondly by all whose lives you have touched. <br>Love,'
+}, {
+  author: 'Patrick', // Lawler
+  date: 'December 27th, 2017',
+  text: 'Dear Gareth,<br>I have so enjoyed talking with you at the Cosmos Club over the past couple of years. Thanks particularly for introducing me to the question of Shakespeare authorship. Your conversation has always been scintillating. Many sincere thanks.'
+}, {
+  author: 'Bob', // Meyers
+  date: 'December 27th, 2017',
+  text: 'Gareth,<br>thank you so much for starting the Shakespeare Authorship Group at the Cosmos Club. I have learned what a battle it was and over time I\'ve come to appreciate that more and more! Thank you also for getting me involved with the group -- I thought I was retiring but that turned out not to be the case. So much to admire about you and we are all in your debt.'
+}, {
+  author: 'Wally and Maria', // Hurst
+  date: 'December 27th, 2017',
+  text: 'My dear friend Gareth,<br>Thank you for allowing me to know you, although the time has been all too brief for our get-togethers in person.<br>Maria and I want you to know that our thoughts and prayers are with you at this time. We thank you for your hospitality and kindness in sharing our love of Shakespeare and the authorship question with you and so many others.<br>Our best to your wonderful family as well.<br>Love,'
+}, {
+  author: 'Mark', // Olshaker
+  date: 'December 27th, 2017',
+  text: 'Dear Gareth,<br>We are thinking of you continually and treasuring your presence in our lives. And I find it difficult to look at a Shakespeare play or commentary without your image arising.'
+}, {
+  author: 'Joan and Malcolm', // McArthur
+  date: 'December 27th, 2017',
+  text: 'Dear Gareth, Amy, Llewellyn and Rhys,<br>You are all in my thoughts and prayers. I hold fond memories of Gareth\'s leadership at the ILO, NY especially during the difficult time of \'911\' - we all hurt together at that horrible sight from our office window, and Gareth was the stalwart among us to get us through the trauma of that day and the days beyond. <br>It was our good fortune to meet Amy and the boys and to share dinner at the Howells in upstate NY.<br>I will keep all of you near in heart as I send positive thoughts to you.'
+}, {
+  author: 'Elisabeth', // Waugaman
+  date: 'December 26th, 2017',
+  text: 'Gareth, Thank you for enlightening us with your seemingly limitless knowledge and your willingness to question. Thank you for your British savior-faire and your Celtic independence. And let\'s not forget those great bow-ties! You have enriched our lives in so many ways. We are holding you and your wonderful family in our thoughts and prayers.'
+},
+// {u'message': u'Annwyl Gareth, Amy, Llewelyn a Rhys,\nMae\u2019r ddau ohonom yn gweddio drosoch chi fel teulu bob dydd. \nPan yn meddwl amdanat ti, y peth sydd yn selfyll allan yw ail-gwrdd a ti yn ystod yr Wyl Gymraeg (NAFOW) yn Cincinnati 2006. Rhwy\u2019n cofio\u2019n eglir i fi eistedd gyda ti a Emir Jones Parry  yn cael seibiant bach ac i ni dechrau, un ar ol y llall,  rhoi allan llinell gyntaf o emyn cymraeg ar ddau arall, os gallent, mynd ymlaen ar ail llinell. Oedd hi\u2019n rhyfedd y nifer o emynau i ni drafod yn ystod hanner awr neu mwy! Mae un pennill yn arbenig yn dod yn ol i fi nawr ac efallai bydd o rhyw galondid:\n\nMae llais y gwylwyr oddi draw\nYn dweud bod bore llon ger llaw\nFod cymyl nos yn cilio mhell\nO flaen dylanwad bywyd gwell\n\nYn ystod y blynyddoedd ers i ti ar teulu symyd i \u201cDC,\u201d oedd yn bleser  cael dy gwmni. Rhoedd Cymdaethas Dewi Sant yn fodus cael ti fel llywydd ac hefyd yr WNAA cael ti fel ymddiriedolwr. Os oes rhwbeth allwn ni wneud i rhoi cymorth i ti neu\u2019r teilu, gofyna i Amy rhoi galwad  (240) 731-5031 cell.\n \nLlawer o gariad a phob bendith, Hywel a Mari\n', u'name': u'Hywel a Mari', u'email': u''}
+{
+  author: 'Rhitu',
+  date: 'December 26th, 2017',
+  text: 'Dear Mr Howell,<br>Thank you for being the most amazing boss and mentor. I am eternally grateful for the time spent with us- to help us grow and learn in such complex times. These skills are now the basis of most of my work.  I have such fond memories with your family and Genghis as well. Our work team was another family. The surprise birthday parties in the office- where one time the singing telegram turned out to be a gorilla and a fake police officer in our conference room that unfortunately scared everyone due to the Israeli protests happening down the street at the same time. <br>Thank you for sharing your leadership and advice.'
+}, {
+  author: 'Christine',
+  date: 'December 26th, 2017',
+  text: 'Gareth is the ONLY person who can burst into song -- specifically Lyoba -- and make it sound as though he grew up singing it on the Swiss mountaintops. We are thankful for the wonderful times we have shared together from watching the pig races in Jussy to savoring Gareth\'s best remedy for colds (whiskey, whiskey, lemon and cloves). <br>There are few raconteurs to match Gareth\'s warmth, humor and joy!<br>Sending our love,<br>Joe and Christine'
+}, {
+  author: 'Lorraine, Charlie, Elizabeth and Matthew',
+  date: 'December 26th, 2017',
+  text: 'Dear Amy and Gareth,<br>Even though your years in Larchmont were few, they were impactful for the The Henry and Forenza Families. We were so lucky to have the boys be friends, have play dates with dinners, Halloween outings and mini vacations together.  Gareth, every single dinner we have with friends, we think and speak of you\u2014every single one! Charlie can not help himself to start to clear the table while people are still eating. You would plead with him to sit down and enjoy the food and wine and company and not rush the meal! You even offered to do all the dishes yourself, if he would learn to relax, just a bit. Such great advice we are reminded of all the time. Love you all, Godspeed,'
+}, {
+  author: 'Lisa', // Steinberg
+  date: 'December 25th, 2017',
+  text: 'Gareth,<br>I always admired the way you made up your own mind about me after a mom at our sons\u2019 school spread nasty rumors about me.<br>As a result, you and I enjoyed many pleasant chats on the sidelines while your Rhys & my Dan played soccer.<br>And you are still the only person I\u2018ve ever met in America who knows about the Mitfords.😊😊<br>Glad I know you and glad that I was able to spend time with you.<br>Sending warm & healing thoughts your way.'
+}, {
+  author: 'Walid',
+  date: 'December 25th, 2017',
+  text: 'Gareth, Amy, Rhys, Llewelyn:<br>Wishing you all a Merry Christmas and a Happy New Year. Awesome pictures!'
+}, {
+  author: 'Anne & Jeff',
+  date: 'December 24th, 2017',
+  text: 'Dear Gareth, Amy, Llewelyn and Rhys,<br>We are sending our love to you all and pray daily for God\'s comfort and peace to surround you. <br>With love and friendship,'
+}, {
   author: 'Suzanne and Chuck',
   date: 'December 24th, 2017',
   text: '&#x1f495;&#128591; feel our sweet prayers to Gareth and everyone surrounding him near and far, seen and unseen. So many wonderful memories of the babes - Llewelyn, Sam, and Alex born within 12 days of each other and all of us "old folks" becoming parents together! Will dig out some pictures especially of our time in the Adirondacks at Claire\'s with Amy and Suzanne climbing High Peaks and Chuck and Gareth holding fort/forth on the couch - napping, reading, talking and solving the world\'s problems! Then marvelous dinners, wine, conversations and young children asleep!'
@@ -23,11 +77,11 @@ export default [{
   date: 'December 23rd, 2017',
   text: 'Annwyl Gareth, Amy, Llewelyn a Rhys,<br>I am so grateful to you for giving me one of the best experiences and opportunities of my life.  My time with you was the best! I loved being a part of your family - you set me up for parenthood! I send you lots of cariad. I will dig out some photos and share them to make you smile. Cariad mawr atoch chi Gareth, a diolch am brofiad anhygoel.<br>xxxx'
 }, {
-  author: 'Jamie',
+  author: 'Jamie', // Nelson
   date: 'December 23rd, 2017',
-  text: 'Wonderful photos.  Still can recall like its yesterday Gareth showing up at our house early one Saturday morning to take David for a walk around Cranberry Lake with the boys.  I immediately asked if I could join the expedition. And that was the start of a long and very valued friendship.<br>Much love, Jamie'
+  text: 'Wonderful photos. Still can recall like its yesterday Gareth showing up at our house early one Saturday morning to take David for a walk around Cranberry Lake with the boys.  I immediately asked if I could join the expedition. And that was the start of a long and very valued friendship.<br>Much love,'
 }, {
-  author: 'Mona',
+  author: 'Mona', // Jonhston
   date: 'December 22nd, 2017',
   text: 'Loved the pictures! Thinking of you all and hoping you enjoy your time together this holiday season.'
 }, {
@@ -35,7 +89,7 @@ export default [{
   date: 'December 22nd, 2017',
   text: 'I sent a photo of Sylvie\'s birthday party in Thier where we met 20 years ago. It was a magical beginning of friendship.'
 }, {
-  author: 'Chris',
+  author: 'Chris', // Mooney
   date: 'December 22nd, 2017',
   text: 'We think back to those early years of Brendon and Rhys in Montessori and elementary school, and the wonderful times we shared with your family. I think about our sweaty, dirty boys and the fun experiences they had and sitting in your back yard in Larchmont watching them play and enjoying your company.<br>Sending peace and much love to all of you. <br>Chris, Christine, Brendon, Kevin, Cayley and baby Caiden'
 }, {
